812d7dcc6b95113197ee899d367a1ece6a6ece56
Yau1heni <lodbrok9@gmail.com>
2023-05-26 14:27:02 +0300

    feat: added a page empty state for vacancies and favourites

--- src/components/vacancies/vacancy-favorites/vacancy-favorites.tsx
+++ src/components/vacancies/vacancy-favorites/vacancy-favorites.tsx
@@ -2,11 +2,19 @@ import { useLocalStorage } from '@mantine/hooks'
 import { VacancyCard } from '@/components/vacancies/vacancy/vacancy-card/vacancy-card'
 import { VacancyType } from '@/services/vacancies-service'
 import styles from './vacancy-favorites.module.css'
+import { useRouter } from 'next/router'
+import { RouteNames } from '@/consts/routes'
 
 export const VacancyFavorites = () => {
   const [value] = useLocalStorage<VacancyType[]>({
     key: 'favorites',
   })
+
+  const { push } = useRouter()
+
+  if (value?.length === 0) {
+    push(RouteNames.EMPTY_STATE)
+  }
 
   const favoritesList = value?.map((favorites) => (
     <VacancyCard key={`ls${favorites.id}`} vacancy={favorites} />
