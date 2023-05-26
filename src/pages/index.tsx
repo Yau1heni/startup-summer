8752007d7b37d6475ca1a94bf5f90dca9bd7465f
Yau1heni <lodbrok9@gmail.com>
2023-05-26 16:42:36 +0300

    fix: corrected the initial display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import { getLayout } from '@/components/layout/layout'
 import { NextPageWithLayout } from '@/pages/_app'
+import { useEffect } from 'react'
+import { RouteNames } from '@/consts/routes'
+import { useRouter } from 'next/router'
 
 const Home: NextPageWithLayout = () => {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    push(RouteNames.VACANCIES).then()
+  })
+
   return (
     <Head>
       <title>startup summer</title>
-      <meta name="description" />
-      <link rel="icon" href="public/favicon.ico" />
+      <meta name="jobored" />
     </Head>
   )
 }
